c92878ba3975c49aa8c07c51804b327a729a9586
n.avontuur <n.avontuur@outlook.com>
2023-10-14 17:11:04 +0200

    Optimize with chatGPT

--- mini_project/mini_project/vision_sender_node.py
+++ mini_project/mini_project/vision_sender_node.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-import rclpy
-from rclpy.node import Node
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
-import cv2
-import depthai
-
 class vision_sender_node(Node):
     def __init__(self):
         super().__init__("vision_node")
@@ -13,44 +5,41 @@ class vision_sender_node(Node):
         self.cam = self.create_publisher(Image, "/cam", 10)
         self.image_msg = Image()
         self.get_logger().info("vision node started")
+        self.device = self.init_depthai_device()
 
-    def cameraOutput(self):
-        # Creating empty "pipeline" object
+    def init_depthai_device(self):
         pipeline = depthai.Pipeline()
-
-        # Add camera to pipeline with definition of previewSize and InterLeaved
         cam_rgb = pipeline.create(depthai.node.ColorCamera)
         cam_rgb.setPreviewSize(300, 300)
         cam_rgb.setInterleaved(False)
-
-        # Receive camera frame by XLink communication
         xout_rgb = pipeline.create(depthai.node.XLinkOut)
         xout_rgb.setStreamName("rgb")
         cam_rgb.preview.link(xout_rgb.input)
-
-        # Create USB2 connection, not the default USB3 (Lower bandwidth, fewer issues)
         device = depthai.Device(pipeline, usb2Mode=True)
+        return device
 
-        # Define host side output queues to access the product results
-        q_rgb = device.getOutputQueue("rgb")
+    def cameraOutput(self):
+        q_rgb = self.device.getOutputQueue("rgb")
 
-        while True:  # Continuous image capture and publishing
+        while rclpy.ok():
             in_rgb = q_rgb.tryGet()
             if in_rgb is not None:
                 frame = in_rgb.getCvFrame()
                 self.image_msg = self.bridge.cv2_to_imgmsg(frame, "bgr8")
-                # Set the timestamp of the Image message
                 self.image_msg.header.stamp = self.get_clock().now().to_msg()
                 self.cam.publish(self.image_msg)
-                self.get_logger().info("Image sent") 
-
+                self.get_logger().info("Image sent")
 
 def main(args=None):
     rclpy.init(args=args)
     node = vision_sender_node()
-    node.cameraOutput()
-    rclpy.spin(node)
-    rclpy.shutdown()
+    try:
+        node.cameraOutput()
+    except Exception as e:
+        node.get_logger().error(f"An error occurred: {e}")
+    finally:
+        node.device.close()  # Close the DepthAI device
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
