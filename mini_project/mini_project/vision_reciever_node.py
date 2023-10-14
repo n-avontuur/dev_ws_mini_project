a80545bfc77d2ce3c95335550bb7dfa6ae06afbd
n.avontuur <n.avontuur@outlook.com>
2023-10-14 18:04:09 +0200

    Now cv2 shoms images

--- mini_project/mini_project/vision_reciever_node.py
+++ mini_project/mini_project/vision_reciever_node.py
@@ -5,14 +5,16 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
 
-class vision_reciever_node(Node):
+class vision_receiver_node(Node):
     def __init__(self):
         super().__init__("vision_node")
         self.images = self.create_subscription(Image, "/cam", self.image_callback, 10)
         self.image_msg = Image()
         self.get_logger().info("vision node started")
-        self.timer = self.create_timer(0.1, self.display_image)  # Adjust the period as needed
         self.bridge = CvBridge()
+
+        # Create a window for image display
+        cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
 
     def image_callback(self, msg):
         self.get_logger().info("Image received")
@@ -22,16 +24,26 @@ class vision_reciever_node(Node):
         if self.image_msg is not None:
             try:
                 cv_image = self.bridge.imgmsg_to_cv2(self.image_msg, desired_encoding="bgr8")
-                # Your image processing or display code here
+                cv2.imshow("Image", cv_image)  # Display the image
+                cv2.waitKey(1)  # Update the display (1 millisecond)
             except CvBridgeError as e:
                 self.get_logger().error(f"Error converting Image message: {e}")
 
+    def cleanup(self):
+        # Close the OpenCV window when the node shuts down
+        cv2.destroyAllWindows()
 
 def main(args=None):
     rclpy.init(args=args)
-    node = vision_reciever_node()
-    rclpy.spin(node)
-    rclpy.shutdown()
+    node = vision_receiver_node()
+    
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.cleanup()  # Ensure the window is closed on node shutdown
+        rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
