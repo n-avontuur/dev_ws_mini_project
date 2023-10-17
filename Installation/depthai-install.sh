700c54ee07c272918a15ca61a5c6b0f762d1098d
n.avontuur <n.avontuur@outlook.com>
2023-10-17 10:36:54 +0200

    moved install files to own folder

---
+++ Installation/depthai-install.sh
@@ -0,0 +1,4 @@
+echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="03e7", MODE="0666"' | sudo tee /etc/udev/rules.d/80-movidius.rules
+sudo udevadm control --reload-rules && sudo udevadm trigger
+
+sudo apt install ros-humble-depthai-ros
