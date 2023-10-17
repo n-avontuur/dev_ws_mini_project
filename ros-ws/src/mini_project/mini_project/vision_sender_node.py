be9e8c0a700ff2981a00d5a714b11173d732e15e
n.avontuur <n.avontuur@outlook.com>
2023-10-17 17:22:46 +0200

    Added imshow to see what gets published

--- ros-ws/src/mini_project/mini_project/vision_sender_node.py
+++ ros-ws/src/mini_project/mini_project/vision_sender_node.py
@@ -3,13 +3,15 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import depthai
+import cv2
 
 class Vision_Sender_Node(Node):
     def __init__(self):
         super().__init__("vision_node")
         self.bridge = CvBridge()
-        self.cam = self.create_publisher(Image, "/cam", 10)
         self.device = self.init_depthai_device(rgb_fps=15, resolution=(1920, 1080))  # Set the desired FPS for the RGB camera
+        self.viewer_window_name = "Image Viewer"
+        cv2.namedWindow(self.viewer_window_name, cv2.WINDOW_NORMAL)
 
     def init_depthai_device(self, rgb_fps, resolution):
         pipeline = depthai.Pipeline()
@@ -38,8 +40,11 @@ class Vision_Sender_Node(Node):
                 frame = in_rgb.getCvFrame()
                 image_msg = self.bridge.cv2_to_imgmsg(frame, "bgr8")
                 image_msg.header.stamp = self.get_clock().now().to_msg()
-                self.cam.publish(image_msg)
-                self.get_logger().info("Image sent")
+                self.cam.publish(image_msg)  # Publish the image
+
+                # Display the image in the viewer window
+                cv2.imshow(self.viewer_window_name, frame)
+                cv2.waitKey(1)  # Update the display (1 millisecond)
 
 def main(args=None):
     rclpy.init(args=args)
