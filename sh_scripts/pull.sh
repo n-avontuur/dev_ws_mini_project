8e95a466721e8199db7e0a39657e345cb16938cc
n.avontuur <n.avontuur@outlook.com>
2023-10-21 14:49:52 +0200

    Added folder for dev_scripts, with script in it

---
+++ sh_scripts/pull.sh
@@ -0,0 +1,6 @@
+cd ~/dev_ws_mini_project
+git pull
+cd ~/dev_ws_mini_project/ros-ws
+colcon build
+source install/setup.bash
+cd ~/dev_ws_mini_project/
---
+++ sh_scripts/testPublisherNode.sh
@@ -0,0 +1,4 @@
+cd ~/dev_ws_mini_project/ros-ws
+colcon build
+source install/setup.bash
+ros2 run mini_project objectdetection_sender_node
