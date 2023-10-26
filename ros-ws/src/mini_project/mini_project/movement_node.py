46494d82a67505b4c3cb8b049f155a99d0b5c7d5
n.avontuur <n.avontuur@outlook.com>
2023-10-26 15:03:22 +0200

    Update some small things

--- ros-ws/src/mini_project/mini_project/movement_node.py
+++ ros-ws/src/mini_project/mini_project/movement_node.py
@@ -9,8 +9,8 @@ class movement_node(Node):
     def __init__(self):
         super().__init__('movement_node')
         self.bridge = CvBridge()
-        # self.image_subscription = self.create_subscription(
-        #    Image, 'camera_image', self.image_callback, 10)
+        self.image_subscription = self.create_subscription(
+            Image, 'camera_image', self.image_callback, 10)
         self.object_info_subscription = self.create_subscription(
             CustomObjectInfo, 'tracked_objects_info', self.object_info_callback, 10)
         self.cmd_pub = self.create_publisher(Twist, 'cmd_vel', 10)
@@ -18,31 +18,11 @@ class movement_node(Node):
 
     def object_info_callback(self, msg):
         if msg is not None:
-            print("object info re")
-            obj_id = int(msg.id)
+            obj_id = msg.id
             label = msg.label
             status = msg.status
 
             if status == "removed":
-<<<<<<< HEAD
-                # If an object with this ID exists, remove it from the dictionary.
-                if obj_id in self.detected_objects:
-                    del self.detected_objects[obj_id]
-            else:
-                # Store the object information in the dictionary using the ID as the key.
-                if obj_id not in self.detected_objects:
-                    self.detected_objects[obj_id] = {
-                        "label": label,
-                        "status": status,
-                        "id": int(obj_id),
-                        "x1": float(msg.x1),
-                        "y1": float(msg.y1),
-                        "x2": float(msg.x2),
-                        "y2": float(msg.y2),
-                        "depth_value": float(msg.depth_value)
-                    }
-        self.move_turtlebot()
-=======
                 # Remove the object with the specified ID from the dictionary if it exists.
                 if obj_id in self.detected_objects:
                     del self.detected_objects[obj_id]
@@ -58,59 +38,31 @@ class movement_node(Node):
                     "y2": msg.y2,
                     "depth_value": msg.depth_value
                 }
->>>>>>> 9a6e845 (Update some small things)
 
     def image_callback(self, msg):
-        self.move_turtlebot()
-
-    def move_turtlebot(self):
         try:
             if self.detected_objects:
                 # Initialize variables to keep track of the closest object and its depth.
                 closest_obj = None
-<<<<<<< HEAD
-                min_depth = float('inf')
-=======
                 min_distance = float('inf')
->>>>>>> 9a6e845 (Update some small things)
 
                 for obj_id, obj_data in self.detected_objects.items():
                     depth = obj_data['depth_value']
 
                     # Check if the current object is closer than the previously found closest object.
-<<<<<<< HEAD
-                    if depth < min_depth:
-                        min_depth = depth
-=======
                     if depth < min_distance:
                         min_distance = depth
->>>>>>> 9a6e845 (Update some small things)
                         closest_obj = obj_data
 
                 if closest_obj is not None:
                     x1 = closest_obj['x1']
                     x2 = closest_obj['x2']
-<<<<<<< HEAD
-
-                    # Calculate the desired goal position based on the closest object's depth.
-                    goal_depth = min_depth
-
-                    # Check if the closest object is on the left or right side based on x1 and x2.
-                    if (x1 + x2) / 2 < 0.5:
-                        # Object is on the left side
-                        goal_x = x1  # Use x1 as the goal_x
-                    else:
-                        # Object is on the right side
-                        goal_x = x2  # Use x2 as the goal_x
-
-=======
                     depth = closest_obj['depth_value']
 
                     # Calculate the desired goal position based on the closest object's depth and position.
                     goal_x = (x1 + x2) / 2
                     goal_depth = depth
 
->>>>>>> 9a6e845 (Update some small things)
                     # Calculate linear and angular velocities to navigate towards the closest object.
                     cmd_msg = Twist()
                     cmd_msg.linear.x = 0.1 * (goal_depth - 1.0)  # Adjust linear velocity based on depth
@@ -132,6 +84,7 @@ class movement_node(Node):
 
         except Exception as e:
             self.get_logger().error(f"Error processing image: {str(e)}")
+
 
 def main(args=None):
     rclpy.init(args=args)
