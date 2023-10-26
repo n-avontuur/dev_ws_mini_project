23108376ac86ee56bb287de7e61687756ef19fe5
n.avontuur <n.avontuur@outlook.com>
2023-10-26 20:01:49 +0200

    remove one callback

--- ros-ws/src/mini_project/mini_project/movement_node.py
+++ ros-ws/src/mini_project/mini_project/movement_node.py
@@ -14,7 +14,7 @@ class movement_node(Node):
     def __init__(self):
         super().__init__('movement_node')
         self.bridge = CvBridge()
-        self.image_subscription = self.create_subscription(Image, 'camera_image', self.image_callback, 10)
+        #self.image_subscription = self.create_subscription(Image, 'camera_image', self.image_callback, 10)
         self.cmd_pub = self.create_publisher(Twist, '/cmd_vel', 10)
         self.object_info_subscription = self.create_subscription( CustomObjectInfo, 'tracked_objects_info', self.object_info_callback, 10)
         self.detected_objects = {}
